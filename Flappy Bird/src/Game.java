bf46bcec45bdf04639af4919832fe720ced04503
Hawoline <belikhawoline@gmail.com>
2021-01-09 01:03:14 +0800

    Code formatted

--- Flappy Bird/src/Game.java
+++ Flappy Bird/src/Game.java
@@ -7,63 +7,55 @@ import processing.core.PVector;
 
 public class Game {
 
-	PApplet p;
-	Pipes pipes;
-	Population population;
-	boolean trainMode = false;
-	Menu menu;
-	
-	public Game(PApplet p, Menu menu) {
-		this.p = p;
-		this.menu = menu;
-		
-		
-	}
+    PApplet p;
+    Pipes pipes;
+    Population population;
+    boolean trainMode = false;
+    Menu menu;
 
-    public void setup(){
-    	pipes = new Pipes(p);
-    	population = new Population(p);
+    public Game(PApplet p, Menu menu) {
+        this.p = p;
+        this.menu = menu;
+
 
     }
 
-    public void display(){
-    	p.background(0);
-    	
-		population.setPipes(pipes.getPipes());
-    	pipes.display();
-    	population.display();
-    	
-    	if(population.isAllBirdsDie()) {
-    		pipes.reset();
-    	}
-    	population.update();	
-    	
-    	menu.display();
-    	menu.setGeneration(population.getGeneration());
-    	menu.setScore(population.getScore());
-    	
-    	
+    public void setup() {
+        pipes = new Pipes(p);
+        population = new Population(p);
+
     }
-    
+
+    public void display() {
+        p.background(0);
+
+        population.setPipes(pipes.getPipes());
+        pipes.display();
+        population.display();
+
+        if (population.isAllBirdsDie()) {
+            pipes.reset();
+        }
+        population.update();
+
+        menu.display();
+        menu.setGeneration(population.getGeneration());
+        menu.setScore(population.getScore());
+
+
+    }
+
     public void trainMode() {
-    	while(trainMode) {
-    		p.background(0);
-        	population.setPipes(pipes.getPipes());
-        	pipes.display();
-        	population.display();
-        	
-        	if(population.isAllBirdsDie()) {
-        		pipes.reset();
-        	}
-        	population.update();	
-    	}
-    }
-    
- 
-    
-	
-	
-	
-	
+        while (trainMode) {
+            p.background(0);
+            population.setPipes(pipes.getPipes());
+            pipes.display();
+            population.display();
 
+            if (population.isAllBirdsDie()) {
+                pipes.reset();
+            }
+            population.update();
+        }
+    }
 }
--- Flappy Bird/src/Main.java
+++ Flappy Bird/src/Main.java
@@ -6,57 +6,48 @@ import processing.core.PVector;
 
 public class Main extends PApplet {
 
-	Game game;
-	Menu menu;
-	int speed = 1;
-	
-	public static void main(String[] args) {
-		PApplet.main("Main");
-		
-	}
+    Game game;
+    Menu menu;
+    int speed = 1;
 
-	public void settings(){
-		size(1200,800);
+    public static void main(String[] args) {
+        PApplet.main("Main");
+
     }
 
-    public void setup(){
-    	menu = new Menu(this);
-    	game = new Game(this, menu);
-    	game.setup();
-    	frameRate(999);
+    public void settings() {
+        size(1200, 800);
     }
 
-    public void draw(){
-    	background(0);
-    	game.display();
-    	
+    public void setup() {
+        menu = new Menu(this);
+        game = new Game(this, menu);
+        game.setup();
+        frameRate(999);
     }
-    
-    
-    
+
+    public void draw() {
+        background(0);
+        game.display();
+    }
+
     public void keyPressed() {
-    	if (key == 't' || key == 'T') {
-    		System.out.println("Instant Score: " + game.population.frameCount);
-    		 Thread thread = new Thread(){
-    			    public void run(){
-    			     game.trainMode();
-    			    }
-    		 }; thread.start();
-    		
-    		if(game.trainMode) {
-    	    	  loop();
-    	    	  game.trainMode = false;
-    	      } else {
-    	    	  noLoop();
-    	    	  game.trainMode = true;
-    	      }
-    	}   	
-    }
-    
-    
-	
-	
-	
-	
+        if (key == 't' || key == 'T') {
+            System.out.println("Instant Score: " + game.population.frameCount);
+            Thread thread = new Thread() {
+                public void run() {
+                    game.trainMode();
+                }
+            };
+            thread.start();
 
+            if (game.trainMode) {
+                loop();
+                game.trainMode = false;
+            } else {
+                noLoop();
+                game.trainMode = true;
+            }
+        }
+    }
 }
